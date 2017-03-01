dc630ddcd58016ac14b1dd0732028b1e78b8f037
felipe <felipe.basina@gmail.com>
2017-03-01 11:15:14 -0300

    Definido modelo e migration inicial

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from collection.models import Thing
 
 # Register your models here.
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)} # Definindo o valor padrao para 'slug'
+
+admin.site.register(Thing, ThingAdmin)
