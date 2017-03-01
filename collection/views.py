ce5452422441ec5bbd708dc980d9773f12241460
felipe <felipe.basina@gmail.com>
2017-03-01 11:27:35 -0300

    Adicionado queryset para recueperar registros; exibicao de registros recuperados

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 def index(request):
+    '''
     number = 7
     coisa = "coisa..."
     return render(request, 'index.html', { 'number': number,
                                             'coisa': coisa, })
+    '''
+    things = Thing.objects.all()
+    #things = Thing.objects.filter(name__contains="Hello")
+    
+    return render(request, 'index.html', { 'things': things, })
