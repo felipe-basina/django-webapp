0a48a7880d1a24a4266e4ce951dc1958a31e2d60
felipe <felipe.basina@gmail.com>
2017-03-01 14:04:46 -0300

    Adicionado controle para edicao de registros

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from collection.forms import ThingForm
 from collection.models import Thing
 
 def index(request):
@@ -15,6 +17,32 @@ def index(request):
     
 def thing_detail(request, slug):
     # Recuperar o objeto
-    thing = Thing.objects.get(slug=slug)
+    thing = recuperar_objeto_por_slug_param(slug)
     
     return render(request, 'things/thing_detail.html', { 'thing': thing, })
+    
+def edit_thing(request, slug):
+    # Recuperar o objeto
+    thing = recuperar_objeto_por_slug_param(slug)
+    
+    form_class = ThingForm
+    
+    # Caso o formulario seja submetido com o metodo POST
+    if request.method == 'POST':
+        form = form_class(data=request.POST, instance=thing)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('thing_detail', slug=thing.slug) # Redireciona
+    else:
+        form = form_class(instance=thing)
+    
+    return render(request, 'things/edit_thing.html', { 'thing': thing, 
+                                                        'form': form, })
+    
+    
+####################
+# Funcoes diversas #
+####################
+def recuperar_objeto_por_slug_param(slug):
+    return Thing.objects.get(slug=slug)
